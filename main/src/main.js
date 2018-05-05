69e199875ef480eea4da78c5518826ed07850a2e
Ivanq <imachug@yandex.ru>
2018-05-05 18:05:52 +0300

    Update main.js, route.js

--- main/src/main.js
+++ main/src/main.js
@@ -5,6 +5,8 @@ import Vue from "vue/dist/vue.min.js";
 import AsyncComputed from "vue-async-computed";
 Vue.use(AsyncComputed);
 
+Vue.prototype.$eventBus = new Vue();
+
 import root from "./vue_components/root.vue";
 var app = new Vue({
 	el: "#app",
@@ -19,3 +21,11 @@ import {route, zeroPage} from "./route.js";
 route(app);
 
 Vue.prototype.$zeroPage = zeroPage;
+
+(async function() {
+	const siteInfo = await zeroPage.getSiteInfo();
+	app.$eventBus.$emit("setSiteInfo", siteInfo);
+})();
+zeroPage.on("setSiteInfo", msg => {
+	app.$eventBus.$emit("setSiteInfo", msg.params);
+});
--- main/src/route.js
+++ main/src/route.js
@@ -4,20 +4,42 @@ const zf = new ZeroFrame();
 import ZeroPage from "zero-dev-lib/ZeroPage";
 const zp = new ZeroPage(zf);
 
+import ZeroFS from "zero-dev-lib/ZeroFS";
+const zeroFS = new ZeroFS(zp);
+
 import ZeroDB from "zero-dev-lib/ZeroDB";
 const zeroDB = new ZeroDB(zp);
+
+import ZeroAuth from "zero-dev-lib/ZeroAuth";
+const zeroAuth = new ZeroAuth(zp);
+zp.auth = zeroAuth;
 
 import Vue from "vue/dist/vue.min.js";
 import VueRouter from "./libs/vuerouter.js";
 const router = VueRouter(zp);
 Vue.use(router.plugin);
 
+zp.on("wrapperPopState", res => router.router.listenForBack(res.params));
+
 import Routes from "./router_pages/routes.js";
 export const route = vue => {
-	const routes = Routes(vue);
+	const routes = Routes(vue, zp);
 
-	routes.forEach(route => router.router.add(route.path, route.controller));
+	routes.forEach(route => {
+		router.router.add({
+			path: route.path,
+			controller(params) {
+				const oldView = vue.currentView;
+
+				route.controller(params);
+				if(oldView == vue.currentView) {
+					vue.currentView = null;
+					vue.$nextTick(() => vue.currentView = oldView);
+				}
+			}
+		});
+	});
 	router.router.check(router.router.getURL());
 };
 
-export {zp as zeroPage, zeroDB};
+export {zp as zeroPage, zeroDB, zeroFS, zeroAuth};
