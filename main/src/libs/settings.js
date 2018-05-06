6b0532f855ccf7e9b41724dde11dd394e2e96e78
Ivanq <imachug@yandex.ru>
2018-05-06 13:02:53 +0300

    Add Settings::set

--- main/src/libs/settings.js
+++ main/src/libs/settings.js
@@ -8,6 +8,21 @@ class Settings {
 		let settings = content.settings || {};
 		return settings[name] || def;
 	}
+
+	async set(name, value) {
+		let content = await zeroFS.readFile("content.json");
+		content = JSON.parse(content);
+
+		if(!content.settings) {
+			content.settings = {};
+		}
+
+		content.settings[name] = value;
+
+		content = JSON.stringify(content, null, 1);
+		await zeroFS.writeFile("content.json", content);
+		await zeroPage.publish("content.json");
+	}
 };
 
 export default new Settings();
