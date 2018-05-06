bb8bb254588bb4ce130abec497663984a102e836
Ivanq <imachug@yandex.ru>
2018-05-06 21:28:11 +0300

    Sticky footer

--- main/src/router_pages/home/home.vue
+++ main/src/router_pages/home/home.vue
@@ -1,10 +1,25 @@
 <template>
-	<div>
-		<theme-header />
-		<theme-list />
-		<theme-footer />
+	<div class="root">
+		<div class="top">
+			<theme-header />
+			<theme-list />
+		</div>
+		<div class="footer">
+			<theme-footer />
+		</div>
 	</div>
 </template>
+
+<style lang="sass" scoped>
+	.root
+		height: 100%
+		display: flex
+		flex-direction: column
+	.top
+		flex: 1 0 auto
+	.footer
+		flex-shrink: 0
+</style>
 
 <script type="text/javascript">
 	import ThemeHeader from "../../vue_components/theme/header.vue";
