a2f8c561e2c93469f1721fe4c11505b695d10f8e
Ivanq <imachug@yandex.ru>
2018-05-06 09:57:09 +0300

    Add empty 'Posts' page

---
+++ main/src/router_pages/admin/posts.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="posts">
+		<span>1</span>
+	</div>
+</template>
+
+<style lang="sass" scoped>
+</style>
+
+<script type="text/javascript">
+	export default {
+		name: "admin-posts"
+	};
+</script>
--- main/src/router_pages/admin/root.vue
+++ main/src/router_pages/admin/root.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="root">
 		<admin-sidebar />
+
+		<component :is="currentTab"></component>
 	</div>
 </template>
 
@@ -12,8 +14,32 @@
 <script type="text/javascript">
 	import AdminSidebar from "./sidebar.vue";
 
+	import AdminPosts from "./posts.vue";
+	const views = {
+		posts: AdminPosts
+	};
+
 	export default {
 		name: "admin-root",
+		data() {
+			return {
+				currentTab: AdminSidebar
+			};
+		},
+
+		mounted() {
+			this.$eventBus.$on("navigate", this.navigated);
+		},
+		destroyed() {
+			this.$eventBus.$off("navigate", this.navigated);
+		},
+
+		methods: {
+			navigated() {
+				this.currentTab = views[this.$router.currentParams.page || "dashboard"];
+			}
+		},
+
 		components: {
 			"admin-sidebar": AdminSidebar
 		}
