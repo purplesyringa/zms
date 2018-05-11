46092b5fe19a1ef79abf476a746bf853b9c666c5
Ivanq <imachug@yandex.ru>
2018-05-11 20:53:27 +0300

    Move code to separate file; support SCSS

--- main/src/libs/theme-loader/index.js
+++ main/src/libs/theme-loader/index.js
@@ -1,7 +1,6 @@
-import {zeroPage, zeroFS} from "../../route.js";
+import {zeroPage} from "../../route.js";
 import Vue from "vue/dist/vue.min.js";
 import httpVueLoader from "http-vue-loader";
-import Sass from "sass.js/dist/sass.sync.js";
 
 // Replace fetch() and XMLHttpRequest
 async function setUpFetch() {
@@ -18,47 +17,11 @@ async function setUpFetch() {
 	};
 }
 
+import {configCss} from "./css.js";
 (async function() {
 	await setUpFetch();
 
-
-	// Upload all CSS files to Sass
-	let cssFiles = await zeroFS.readDirectory("theme");
-	cssFiles = cssFiles.filter(name => /\.(css|sass|scss)$/.test(name));
-	await new Promise(resolve => Sass.preloadFiles("theme", "", cssFiles, resolve));
-
-	httpVueLoader.langProcessor.sass = code => {
-		// First, remove empty lines
-		code = code.split("\n").filter(line => line.trim()).join("\n");
-		// Now, find out smallest padding
-		let padding = code
-			.split("\n") // Split into lines
-			.map(line => line.match(/^(\s*)/)[0]) // Get padding
-			.map(spaces => spaces.length) // Get padding width
-			.reduce((a, b) => Math.min(a, b), Infinity); // Find least width
-		// If no spaces were found, think of them as 0
-		if(padding === Infinity) {
-			padding = 0;
-		}
-		// Remove spaces
-		code = code.split("\n").map(line => line.substr(padding)).join("\n");
-		// Yay! We need those 10 lines of code to fix stupid {} error!
-
-		let id = Math.random().toString(36).substr(2);
-
-		return new Promise((resolve, reject) => {
-			Sass.writeFile(`.${id}.sass`, code, () => {
-				Sass.compileFile(`.${id}.sass`, result => {
-					if(result.status) {
-						console.log(result);
-						return reject(new Error(result.formatted));
-					}
-
-					resolve(result.text);
-				});
-			});
-		});
-	};
+	await configCss();
 
 	console.log(await httpVueLoader("theme/list.vue"));
 })();
