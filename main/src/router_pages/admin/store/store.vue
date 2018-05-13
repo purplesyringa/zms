5bf5e066c18d630cfa3b7fc9190d7460aee4cd21
Ivanq <imachug@yandex.ru>
2018-05-13 15:09:45 +0300

    Remove useless code

--- main/src/router_pages/admin/store/store.vue
+++ main/src/router_pages/admin/store/store.vue
@@ -20,61 +20,7 @@
 </style>
 
 <script type="text/javascript">
-	import {zeroAuth} from "../../../route.js";
-	import Users from "../../../libs/users.js";
-	import "vue-awesome/icons/edit";
-	import "vue-awesome/icons/caret-square-right";
-
 	export default {
-		name: "admin-users",
-
-		data() {
-			return {
-				newId: "",
-				siteInfo: {
-					settings: {
-						own: false
-					}
-				}
-			};
-		},
-
-		mounted() {
-			this.$eventBus.$on("setSiteInfo", this.setSiteInfo);
-			this.$eventBus.$emit("needSiteInfo");
-		},
-		destroyed() {
-			this.$eventBus.$off("setSiteInfo", this.setSiteInfo);
-		},
-
-		asyncComputed: {
-			async users() {
-				return await Users.getAllRules();
-			},
-
-			async isModerator() {
-				const auth = zeroAuth.getAuth();
-				if(!auth) {
-					return false;
-				}
-
-				return await Users.hasRoleByAddress(auth.address, "moderator");
-			}
-		},
-
-		methods: {
-			setSiteInfo(siteInfo) {
-				this.siteInfo = siteInfo;
-			},
-
-			next() {
-				if(!this.newId || this.newId === "Please, fill in user ID") {
-					this.newId = "Please, fill in user ID";
-					return;
-				}
-
-				this.$router.navigate(`admin/users/change-role/${this.newId}`);
-			}
-		}
+		name: "store"
 	};
 </script>
