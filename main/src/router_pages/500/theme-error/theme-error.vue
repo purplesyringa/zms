142071df42890b0e02c494c31c46c4d9bb412f4a
Ivanq <imachug@yandex.ru>
2018-07-14 13:31:19 +0300

    Show error

---
+++ main/src/router_pages/500/theme-error/theme-error.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="theme-error">
+		<div class="content">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<style lang="sass" scoped>
+	.theme-error
+		display: block
+		width: 100%
+		height: 100%
+
+
+	.content
+		position: absolute
+		left: 20%
+		top: 50%
+		transform: translateY(-50%)
+
+		width: 60%
+
+		font-family: Verdana, Arial, sans-serif
+		font-size: 32px
+		text-align: center
+</style>
+
+<script type="text/javascript">
+	export default {
+		name: "theme-error"
+	};
+</script>
