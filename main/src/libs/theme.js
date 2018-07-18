9456b8f63efc9e84fcd91361ac8a66889a3d1a33
Ivanq <imachug@yandex.ru>
2018-07-18 12:20:44 +0300

    Rebuild themes

--- main/src/libs/theme.js
+++ main/src/libs/theme.js
@@ -2,8 +2,11 @@ import {zeroFS, zeroPage} from "../zero";
 import Settings from "./settings.js";
 import deepcopy from "deepcopy";
 import path from "path";
+import crypto from "crypto";
+import {Buffer} from "buffer";
 import normalizeComponent from "vue-loader/lib/component-normalizer";
 import addStylesClient from "vue-style-loader/lib/addStylesClient";
+import Store from "./store";
 
 import Vue from "vue/dist/vue.min.js";
 
@@ -76,6 +79,110 @@ class Theme {
 
 
 	async loadTheme() {
+		if((await zeroPage.getSiteInfo()).settings.own) {
+			console.log("Checking theme for file changes");
+
+			let manifest;
+			try {
+				manifest = await this.getManifest();
+			} catch(e) {
+				// Malformed manifest
+				return;
+			}
+			const hashes = manifest._hashes;
+			let newHashes = {};
+			let changes = [];
+
+			for(const fileName of Object.keys(hashes)) {
+				let content;
+				try {
+					content = await zeroFS.readFile(`theme/${fileName}`, "arraybuffer");
+				} catch(e) {
+					console.log(fileName, "deleted");
+					changes.push([
+						fileName,
+						"deleted"
+					]);
+					continue;
+				}
+
+				const newHash = crypto.createHash("md5").update(Buffer.from(content)).digest("hex");
+				const oldHash = hashes[fileName];
+
+				if(newHash != oldHash) {
+					console.log(fileName, "changed");
+					changes.push([
+						fileName,
+						"changed"
+					]);
+					newHashes[fileName] = newHash;
+				} else {
+					newHashes[fileName] = newHash;
+				}
+			}
+
+			for(const fileName of await zeroFS.readDirectory("theme", true)) {
+				if(fileName.startsWith("__build/")) {
+					continue;
+				}
+
+				if(!hashes[fileName]) {
+					console.log(fileName, "added");
+					changes.push([
+						fileName,
+						"added"
+					]);
+
+					const content = await zeroFS.readFile(`theme/${fileName}`, "arraybuffer");
+					const newHash = crypto.createHash("md5").update(Buffer.from(content)).digest("hex");
+					newHashes[fileName] = newHash;
+				}
+			}
+
+			if(changes.length) {
+				for(let i = 0; i < changes.length; i++) { // Don't optimize!
+					const [fileName, action] = changes[i];
+					if(manifest._dependents[fileName]) {
+						changes = changes.concat(manifest._dependents[fileName].map(dependency => {
+							return [dependency, "dependency"];
+						}));
+					}
+				}
+
+				console.log("Rebuilding", changes);
+
+				let newDependents = {};
+				for(const [fileName, action] of changes) {
+					if(action === "deleted") {
+						try {
+							await zeroFS.deleteFile(`theme/__build/${fileName}`);
+						} catch(e) {
+							console.warn("[rebuild error]", "Could not delete theme file", fileName);
+						}
+					} else {
+						await Store.mount();
+						const {result: compiled, dependents} = await Store.Themes.rebuildThemeFile(fileName);
+						await zeroFS.writeFile(`theme/__build/${fileName}`, compiled);
+
+						for(const dependentName of Object.keys(dependents)) {
+							if(!newDependents[dependentName]) {
+								newDependents[dependentName] = [];
+							}
+							newDependents[dependentName] = newDependents[dependentName].concat(dependents[dependentName]);
+						}
+					}
+				}
+
+				const newManifest = Object.assign({}, manifest, {
+					_hashes: newHashes,
+					_dependents: Object.assign({}, manifest._dependents, newDependents)
+				});
+				await zeroFS.writeFile("theme/__build/theme.json", JSON.stringify(newManifest, null, "\t"));
+			}
+		}
+
+
+
 		console.log("Loading theme");
 
 		let files = {};
