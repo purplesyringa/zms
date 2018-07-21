ad41b9d6e9ecfcab6055a5a985010d100a0d70a9
Ivanq <imachug@yandex.ru>
2018-07-21 11:24:46 +0300

    Add PluginUtil.DB.query

--- main/src/libs/plugin-util.js
+++ main/src/libs/plugin-util.js
@@ -1,5 +1,12 @@
 import Posts from "./posts";
+import {zeroDB} from "../zero";
 
 export function definePost(f) {
 	Posts.define(f);
 }
+
+export const DB = new class DB {
+	async query(query, args={}) {
+		return await zeroDB.query(query, args);
+	}
+};
