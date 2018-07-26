47b94b4628dae7a054af29ec39c90affba4bffe8
Ivanq <imachug@yandex.ru>
2018-07-26 17:11:08 +0300

    Add PluginUtil.DB.insert

--- main/src/libs/plugin-util.js
+++ main/src/libs/plugin-util.js
@@ -1,6 +1,6 @@
 import Posts from "./posts";
-import {zeroDB} from "../zero";
-import {getPluginTableName} from "./theme";
+import {zeroPage, zeroFS, zeroDB, zeroAuth} from "../zero";
+import {getPluginTableName, getMapOrigin} from "./theme";
 
 export default plugin => {
 	function definePost(f) {
@@ -10,6 +10,36 @@ export default plugin => {
 	const DB = new class DB {
 		async query(query, args={}) {
 			return await zeroDB.query(query, args);
+		}
+
+		async insert(table, row) {
+			const manifest = JSON.parse(await zeroFS.readFile(`plugins/${escape(plugin)}/__build/plugin.json`));
+			if(!manifest.tables || !manifest.tables[table]) {
+				throw new Error(`No '${table}' table defined in plugin.json`);
+			}
+
+			let mapOrigin = getMapOrigin(manifest.tables[table].role);
+			if(mapOrigin.indexOf(".+") > -1) {
+				const auth = await zeroAuth.requestAuth();
+				mapOrigin = mapOrigin.replace(".+", auth.address);
+			}
+
+			if(!zeroPage.isSignable(mapOrigin.replace("data.json", "content.json"))) {
+				throw new Error(`Not allowed to sign ${mapOrigin}`);
+			}
+
+			const autoIncrement = manifest.tables[table].autoincrement;
+
+			return await zeroDB.insertRow(
+				mapOrigin,
+				mapOrigin.replace("data.json", "content.json"),
+				getPluginTableName(plugin, table),
+				row,
+				autoIncrement ? {
+					column: autoIncrement,
+					source: `next__${getPluginTableName(plugin, table)}__${autoIncrement}`
+				} : null
+			);
 		}
 
 		table(tableName) {
